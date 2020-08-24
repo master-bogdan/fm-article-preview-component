4f03df648361cc2782024ff0bf1f112493f9a572
masterbogdan <maiskiychavk@gmail.com>
2020-08-24 10:14:49 +0300

    fix bug on mobile

--- js/script.js
+++ js/script.js
@@ -3,18 +3,20 @@ const shareBar = document.getElementById('share_bar');
 const shareComponent = document.querySelector('.component_user');
 const mobileShareBar = document.getElementById('share_bar_mobile');
 
-shareBtn.addEventListener('mouseover', (event) => {
-    shareBar.style.display = 'block';
+if (window.innerWidth > 375) {
+    shareBtn.addEventListener('mouseover', () => {
+        shareBar.style.display = 'block';
+        
+    });
     
-});
+    shareBtn.addEventListener('mouseout', () => {
+        shareBar.style.display = 'none';
+    });
+}
 
-shareBtn.addEventListener('mouseout', (event) => {
-    shareBar.style.display = 'none';
-});
 
 if (window.innerWidth <= 375) {
-    shareBtn.addEventListener('click', (event) => {
-        event.preventDefault();
+    shareBtn.addEventListener('click', () => {
         shareComponent.style.display = 'none';
         mobileShareBar.style.display = 'flex';
     });
